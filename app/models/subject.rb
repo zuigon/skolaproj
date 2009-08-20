61f2b6d431952688c553a8666dd60f652ecf7196
BKrsta <bkrsta@gmail.com>
2009-08-20 16:06:37 +0200

    Destroy Subjects

--- app/models/subject.rb
+++
@@ -1,8 +0,0 @@
-class Subject < ActiveRecord::Base
-  belongs_to :student
-  #has_many :marks
-  validates_presence_of :name
-  
-  accepts_nested_attributes_for :student, :allow_destroy => true
-  
-end
